77094e2fa8e49b8bcf3231fa6d0f32612688e174
Valentin Bartschies <valentin.bartschies@tuhh.de>
2023-11-12 01:40:03 +0100

    add .env

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import os
+from dotenv import load_dotenv
 
-var = os.environ.get('MY_VAR')
-var2 = os.environ.get('MY_VAR2')
+load_dotenv()
+
+var = os.getenv('MY_VAR')
+var2 = os.getenv('MY_VAR2')
 print("MY_VAR: ", var)
 print("MY_VAR2: ", var2)
+if var2 == None:
+    print("MY_VAR2 is None")
